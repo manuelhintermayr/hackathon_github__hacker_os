f8982c2d4c0ba7ef02dd4a7fda228a11c5368b40
Manuel Hintermayr <manuel@hintermayr.net>
2025-06-08 10:37:22 +0200

    Add Express server setup and package.json for project initialization
    - Created package.json with Express as a dependency and start script
    - Implemented server.js to serve static files and handle root route
    - Configured server to listen on port 3000

---
+++ codeSnippets.js
@@ -0,0 +1,19 @@
+export const codeSnippets = [
+    'function hackSystem() {',
+    '    console.log("Hacking in progress...");',
+    '    let accessGranted = false;',
+    '    for (let i = 0; i < 1000; i++) {',
+    '        if (Math.random() > 0.99) {',
+    '            accessGranted = true;',
+    '            break;',
+    '        }',
+    '    }',
+    '    return accessGranted;',
+    '}',
+    'const result = hackSystem();',
+    'if (result) {',
+    '    console.log("Access granted!");',
+    '} else {',
+    '    console.log("Access denied.");',
+    '}'
+];
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { codeSnippets } from './codeSnippets.js';
+
 new Vue({
     el: '#app',
     data: {
@@ -113,30 +115,11 @@ new Vue({
             simulateSteps();
         },
         simulateDirectHacking() {
-            const codeSnippets = [
-                'function hackSystem() {',
-                '    console.log("Hacking in progress...");',
-                '    let accessGranted = false;',
-                '    for (let i = 0; i < 1000; i++) {',
-                '        if (Math.random() > 0.99) {',
-                '            accessGranted = true;',
-                '            break;',
-                '        }',
-                '    }',
-                '    return accessGranted;',
-                '}',
-                'const result = hackSystem();',
-                'if (result) {',
-                '    console.log("Access granted!");',
-                '} else {',
-                '    console.log("Access denied.");',
-                '}'
-            ];
-
             let index = 0;
             const typeCode = (key) => {
                 if (index < codeSnippets.length) {
-                    this.terminalOutput += `${codeSnippets[index]}\n`;
+                    this.terminalOutput += `${codeSnippets[index]}
+`;
                     this.scrollToBottom();
                     index++;
                 } else {
